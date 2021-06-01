ef7f4956c4a81f1f9c1c764dff54e61b8360fc24
OBX555 <safadevranyuzden@gmail.com>
2021-06-01 20:51:48 +0300

    fsdf

--- test.bat
+++ test.bat
@@ -1,6 +1,12 @@
+set deneme=545646sever
 mkdir isimsiz
 cd .\isimsiz
 mkdir isimsizoglu
 cd .\isimsizoglu
 nul >acmadan_git.txt
 echo !!!beni neden actýn ki , ben cok gereksizim!!! >acmadan_git.txt
+nul >beni_acmadan_gitme.txt
+echo !!!beni surekli ov!!! >beni_acmadan_gitme.txt
+cd..
+cd..
+mkdir %deneme%
